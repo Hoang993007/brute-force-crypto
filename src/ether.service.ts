64a1bd3c256329cc1a8b5becf586316f0f36a82f
Hoang993007 <nguyenminhhoang1999.tg@gmail.com>
2024-05-20 15:47:44 +0700

    fix

--- src/ether.service.ts
+++ src/ether.service.ts
@@ -4,132 +4,9 @@ import {
   StaticJsonRpcProvider,
 } from '@ethersproject/providers';
 import { Injectable } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
-import axios from 'axios';
-import { getChainData } from 'chainlist/axios';
 import * as dotenv from 'dotenv';
 import { shuffle } from 'lodash';
 dotenv.config();
-
-const blacklistRpc = ['https://eth.drpc.org'];
-
-const rpcBody = JSON.stringify({
-  jsonrpc: '2.0',
-  method: 'eth_getBlockByNumber',
-  params: ['latest', false],
-  id: 1,
-});
-
-const formatData = (url, data) => {
-  let height = data?.result?.number ?? null;
-  let latency = data?.latency ?? null;
-  if (height) {
-    const hexString = height.toString(16);
-    height = parseInt(hexString, 16);
-  } else {
-    latency = null;
-  }
-  return { url, height, latency };
-};
-
-const fetchChain = async (baseURL) => {
-  if (baseURL.includes('API_KEY')) return null;
-  try {
-    const API = axios.create({
-      baseURL,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    API.interceptors.request.use(function (request: any) {
-      request.requestStart = Date.now();
-      return request;
-    });
-
-    API.interceptors.response.use(
-      function (response: any) {
-        response.latency = Date.now() - response.config.requestStart;
-        return response;
-      },
-      function (error) {
-        if (error.response) {
-          error.response.latency = null;
-        }
-
-        return Promise.reject(error);
-      },
-    );
-
-    const { data, latency } = (await API.post('', rpcBody)) as any;
-
-    return { data: formatData(baseURL, { ...data, latency }) };
-  } catch (error) {
-    return null;
-  }
-};
-
-const assessChain = (chain: any) => {
-  chain = chain.filter(
-    (el) =>
-      el.data && el.data.height && el.data.latency && el.data.latency < 400,
-  );
-  const sortedData = chain?.sort((a, b) => {
-    const h1 = a?.data?.height;
-    const h2 = b?.data?.height;
-    const l1 = a?.data?.latency;
-    const l2 = b?.data?.latency;
-
-    if (!h2) {
-      return -1;
-    }
-
-    if (h2 - h1 > 0) {
-      return 1;
-    }
-    if (h2 - h1 < 0) {
-      return -1;
-    }
-    if (h1 === h2) {
-      if (l1 - l2 < 0) {
-        return -1;
-      } else {
-        return 1;
-      }
-    }
-  });
-
-  const topRpc = sortedData.find((el) => el.data.latency < 900)?.data ?? {};
-
-  const res = sortedData
-    .map(({ data, ...rest }) => {
-      const { height = null, latency = null, url = '' } = data || {};
-
-      let trust = false;
-      let disableConnect = false;
-
-      if (topRpc.height - height < 1000 && topRpc.latency - latency > -100) {
-        trust = true;
-      } else {
-        trust = false;
-      }
-
-      if (url.includes('wss://') || url.includes('API_KEY'))
-        disableConnect = true;
-
-      const lat = latency ? (latency / 1000).toFixed(3) + 's' : null;
-
-      return {
-        ...rest,
-        data: { ...data, height, latency: lat, trust, disableConnect },
-      };
-    })
-    .filter((el) => el.data.trust);
-
-  console.log(res);
-
-  return res;
-};
 
 @Injectable()
 export class EtherService {
@@ -137,36 +14,6 @@ export class EtherService {
     rpc: ['https://eth.llamarpc.com'],
     chainId: 1,
   };
-
-  // @Cron(CronExpression.EVERY_5_SECONDS)
-  // async getChainDataCronJob() {
-  //   await this.refreshRpc();
-  // }
-
-  async refreshRpc() {
-    console.log('Start refreshing chain data...');
-    const res = await getChainData();
-    const callRes = (
-      await Promise.all(
-        res.rpc
-          .map((el) => {
-            return el.url;
-          })
-          .filter((el) => !!el)
-          .filter((url) => {
-            return !url.includes('wss://') && !blacklistRpc.includes(url);
-          })
-          .map(async (el) => {
-            return await fetchChain(el);
-          }),
-      )
-    ).filter((el) => !!el);
-    const testedChain = await assessChain(callRes);
-    const trustedRpc = testedChain
-      .filter((el) => el.data.trust)
-      .map((el) => el.data.url);
-    this.network = { ...res, rpc: trustedRpc };
-  }
 
   getBatchProvider() {
     const rpc = shuffle(this.network.rpc)[0];
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,6 @@
 import { Module } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ScheduleModule } from '@nestjs/schedule';
-import { ethers, formatUnits } from 'ethers';
-import { extractResToCsv } from 'exportCSV';
-import { writeFileJson } from 'jsonFIle';
-import * as _ from 'lodash';
-import { getResFileName, sleep } from 'utils';
-import { appConfig } from '../app/config';
 import { EtherService } from './ether.service';
 
 @Module({
@@ -14,17 +8,6 @@ import { EtherService } from './ether.service';
   providers: [EtherService],
 })
 export class AppModule {}
-
-// NOTE: NESTJS STANDALONE APPLICATION
-
-// TODO: RPC health check
-// TODO: Ask params before run
-
-// Lisence: give a package, and a very first lisenced
-// Next will be created base on that lisense
-// Run will countdown
-// Print out so user can see
-// Make it into a application
 
 async function bootstrap() {
   const module = await NestFactory.createApplicationContext(AppModule);
@@ -36,75 +19,5 @@ async function bootstrap() {
       '0x5edac8e53e0583e1ebb9ddcd27eeed479bdd6c25dea13ab38442bd5a537b0fe7',
     );
   console.log(res);
-
-  // let count = 0;
-  // const start = Date.now();
-
-  // if (!etherService.network) {
-  //   await etherService.refreshRpc();
-  // }
-
-  // while (!etherService.network) {
-  //   console.log('Waiting for network data...');
-  //   await sleep(3000);
-  // }
-
-  // while (true) {
-  //   const mnemonic = ethers.Mnemonic.fromEntropy(ethers.randomBytes(16));
-  //   console.log('MNEMONIC: ', mnemonic.phrase);
-
-  //   const data = Array.from(Array(appConfig.ADDRESS_NUM).keys()).map(
-  //     (index) => {
-  //       const hdWallet = ethers.HDNodeWallet.fromMnemonic(
-  //         mnemonic,
-  //         `m/44'/60'/0'/0/${index}`,
-  //       );
-  //       return {
-  //         walletAddress: hdWallet.address,
-  //         privateKey: hdWallet.privateKey,
-  //       };
-  //     },
-  //   );
-
-  //   const balances = await Promise.all(
-  //     data.map(async (wallet, index) => {
-  //       let balance;
-
-  //       while (!balance) {
-  //         try {
-  //           console.log(`Chain: ${appConfig.CHAIN} | Account ${index + 1}`);
-  //           console.log('=== Address: ', wallet.walletAddress);
-  //           console.log('=== Private key: ', wallet.privateKey);
-  //           balance = await etherService
-  //             .getProvider()
-  //             .getBalance(wallet.walletAddress);
-  //           return formatUnits(balance.toString(), 'ether');
-  //         } catch (err) {
-  //           console.log('=====> Error in getting balance: retry');
-  //         }
-  //       }
-  //     }),
-  //   );
-
-  //   if (_.flatten(balances).some((el) => Number(el) > 0)) {
-  //     await writeFileJson(
-  //       appConfig.RES_FOLDER_PATH + `/${getResFileName(mnemonic.entropy)}`,
-  //       {
-  //         chain: appConfig.CHAIN,
-  //         seedPhrase: mnemonic.phrase,
-  //         detectedBalance: _.sum(balances.map((el) => Number(el))),
-  //       },
-  //     );
-  //     await extractResToCsv();
-  //     console.log('==========> Balance detected');
-  //   } else {
-  //     console.log('========> No balance found');
-  //   }
-  //   count++;
-  //   console.log('=== Processed: ', count);
-  //   const diff = Date.now() - start;
-  //   console.log('=== Speed: ', (count / (diff / 1000)).toFixed(1), 'wallet/s');
-  //   console.log('\n');
-  // }
 }
 bootstrap();
